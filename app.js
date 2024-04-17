a2d4053542f7f840b3e63ac1e36c3fa2b479772c
Shital Kumar Awal <143069333+shitalgi@users.noreply.github.com>
2024-04-17 23:34:38 +0545

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+//const app = require("express")
 const express = require('express')
 const app = express()
+app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.send("bye world")
+    res.render('home')
+})
+
+
+app.get("/blog",(req,res)=>{
+    res.render(' blog')
+})
+
+
+app.get("/blog/create",(req,res)=>{
+    res.send("soon,here will be the create form")
+})
+app.get("/blog/edit",(req,res)=>{
+    res.send("soon,here will be the edit form")
 })
 
 
